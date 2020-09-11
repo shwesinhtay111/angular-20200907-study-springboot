d0cf4bbed966e71e9f3806cd59fd2580408b193a
ShweSinHtay <shwesin@isgm2.com>
2020-09-11 16:56:55 +0630

    delete todos

--- src/main/java/com/ssh/angular/todo/TodoHardcodedService.java
+++ src/main/java/com/ssh/angular/todo/TodoHardcodedService.java
@@ -19,5 +19,24 @@ public class TodoHardcodedService {
 	public List<Todo> findAll(){
 		return todos;
 	}
+	
+	public Todo deleteById(long id) {
+		Todo todo = findById(id);
+		if(todo==null) return null;
+		
+		if(todos.remove(todo)) {
+			return todo;
+		}
+		return null;
+	}
+
+	private Todo findById(long id) {
+		for(Todo todo:todos) {
+			if(todo.getId() == id) {
+				return todo;
+			}
+		}
+		return null;
+	}
 
 }
